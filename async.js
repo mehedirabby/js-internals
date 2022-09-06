69422cf225a39b2cdc985966e8f525b8ef967f97
unknown <mehedi15-2995@diu.edu.bd>
2022-09-06 21:21:58 +0600

    first commit

---
+++ async.js
@@ -0,0 +1,14 @@
+function doSomething(){
+    console.log('third')
+}
+
+console.log('first')
+console.log('second')
+// setTimeout(()=>{
+//     console.log('third')
+// },5000)
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then(response =>{console.log('response');return response.json()})
+  .then(json => console.log(json))
+console.log('fourth')
+console.log('fifth')
